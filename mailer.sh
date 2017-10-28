fe53e3044e97fdcab8f79c6cec71c40b351326d1
Takács Ákos <rimelek@rimelek.hu>
2017-10-28 18:18:50 +0200

    bugfixes
    - 1. Variable MESSAGE is optional and used when it is not empty
    - 2. Removed second shebang line
    - 3. The sender's name and the message is encoded in UTF-8

--- mailer.sh
+++ mailer.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 
-#!/bin/sh
-
 : ${TO:?Variable TO is required!}
 : ${FROM_EMAIL:?Variable FROM_EMAIL is required!}
 : ${SMTP_USER:?Variable SMTP_USER is required!}
 : ${SMTP_PASS:?Variable SMTP_PASS is required!}
 : ${SMTP_HOST:?Variable SMTP_HOST is required!}
 
-: ${MESSAGE:?Variable Message is required!}
+: ${MESSAGE:-""}
 
 if [ "${TLS}" == "" ]; then TLS=YES; fi;
 if [ "${STARTTLS}" == "" ]; then STARTTLS=YES; fi;
 if [ "${SUBJECT}" == "" ]; then SUBJECT="No Subject"; fi;
-if [ "${FROM_NAME}" == "" ]; then FROM_NAME="${FROM_EMAIL}"; fi;
+if [ "${FROM_NAME}" == "" ]; then FROM_NAME="${FROM_EMAIL}"; else FROM_NAME="=?utf-8?B?"$(echo "${FROM_NAME}" | base64)"?="; fi;
 
 cat > /etc/ssmtp/ssmtp.conf << EOF
 mailhub=${SMTP_HOST}
@@ -32,12 +30,19 @@ EOF
 ENCODED_SUBJECT=$(echo "${SUBJECT}" | base64)
 
 cat > /tmp/ssmtp.txt << EOF
+Content-Type: text/plain; charset=utf-8
 Subject: =?utf-8?B?${ENCODED_SUBJECT}?=
 
 EOF
-
-while read input; do
-  echo $input >> /tmp/ssmtp.txt
-done
+if [ -z "${MESSAGE}" ]; then
+    while read input; do
+        echo $input >> /tmp/ssmtp.txt
+    done;
+else
+    IFS=$'\r\n';
+    for i in ${MESSAGE}; do
+        echo "${i}" >> /tmp/ssmtp.txt
+    done;
+fi;
 
 cat /tmp/ssmtp.txt | ssmtp "${TO}"
